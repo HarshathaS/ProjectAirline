f56cb09f0f0cb0e072e5334effc6032c03c2b325
Harshatha Senthilkumar <harshasenthill@gmail.com>
2025-08-24 15:23:23 +0530

    Delete src/main/java/com/dev/airlinereservationsystem/handler/InvalidCancellationException.java

--- src/main/java/com/dev/airlinereservationsystem/handler/InvalidCancellationException.java
+++
@@ -1,7 +0,0 @@
-package com.dev.airlinereservationsystem.handler;
-
-public class InvalidCancellationException extends RuntimeException{
-    public InvalidCancellationException(String message){
-        super(message);
-    }
-}
